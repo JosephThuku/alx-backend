85acbff94b36bf37609a8704bd0024be87bb9a49
JosephThuku <jthuku490@gmail.com>
2023-07-24 23:34:16 +0300

    1. Simple pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-""" Simple pagination """
+"""
+"""
 import csv
 import math
-from typing import List
+from typing import List, Dict
 
 
 def index_range(page: int, page_size: int) -> tuple:
@@ -39,3 +40,23 @@ class Server:
             return dataset
         except Exception:
             return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """returns a dict of meta data"""
+        data = self.get_page(page, page_size)
+        if len(self.dataset()) % page_size == 0:
+            total_pages = len(self.dataset()) / page_size
+        else:
+            total_pages = len(self.dataset()) / page_size + 1
+        next_page = None if (page + 1) > total_pages else page + 1
+        prev_page = None if page - 1 < 0 else page - 1
+
+        my_dict = {
+            'page': page,
+            'page_size': page_size,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': int(total_pages)
+        }
+        return my_dict
