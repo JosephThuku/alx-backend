2f0da4317065fb02a607b76a58c76a828adcca98
JosephThuku <jthuku490@gmail.com>
2023-08-01 21:12:16 +0300

    0*02 i18 task solution

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,15 +1,35 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+#!/usr/bin/env python3
+"""A simple flask app
+"""
+
+
+from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
 class Config(object):
-    LANGUAGES = ["en", "fr"]
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
+# configure the flask app
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """_summary_
+    """
+    return render_template('1-index.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,19 +1,45 @@
-from flask import Flask, request
+#!/usr/bin/env python3
+"""A simple flask app
+"""
+
+
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
+class Config(object):
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# configure the flask app
 app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
 
-class Config:
-    LANGUAGES = ["en", "fr"]
-
-app.config.from_object(Config)
-babel.default_locale = "en"
-babel.default_timezone = "UTC"
 
 @babel.localeselector
 def get_locale():
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/')
+def index():
+    """_summary_
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
